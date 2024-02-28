ce58fa5225283d2f5818fec3ecc1961512576ca3
Joel Warrington <joel.warrington@shopify.com>
2024-02-28 09:58:39 -0700

    Test #3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,8 +4,14 @@ class Bottles
 
   def verse(num_of_bottles)
     <<~VERSE
-    #{num_of_bottles} bottles of beer on the wall, #{num_of_bottles} bottles of beer.
-    Take one down and pass it around, #{num_of_bottles - 1} bottles of beer on the wall.
+    #{num_of_bottles} #{plural?(num_of_bottles) ? 'bottles' : 'bottle'} of beer on the wall, #{num_of_bottles} #{plural?(num_of_bottles) ? 'bottles' : 'bottle'} of beer.
+    Take one down and pass it around, #{num_of_bottles - 1} #{plural?(num_of_bottles - 1) ? 'bottles' : 'bottle'} of beer on the wall.
     VERSE
+  end
+
+  private
+
+  def plural?(num)
+    num > 1 || num == 0
   end
 end
